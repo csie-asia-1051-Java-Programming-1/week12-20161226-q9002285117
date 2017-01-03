d22a90c921e903747186d0f2b5363e0a6b4c2fab
q9002285117 <105021038@live.asia.edu.tw>
2017-01-04 01:36:35 +0800

    hw

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,11 +1,30 @@
 package hw;
+
+import java.util.Scanner;
+
 /*某遊覽車派遣公司共收到 n 筆任務訂單，訂單中詳細記載發車時間 s 和返回時間 d。 每一輛遊覽車只要任務時間不衝突，可立即更換司機繼續上路執行任務。請問該公司 至少需要調遣多少車輛才足以應付需求? 程式的輸入包含兩行數字，第一行包含一個正整數 n，1 ≤ n ≤ 30，代表第二行有 n 筆 訂單的出發時間和返回時間 s1, d1, s2, d2, ..., sn, dn，0 < si < di ≤ 24，而此 2n 個正整 數間以空格隔開。
 */
 public class hw01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		int data[][] = new int[n][2];
+		int sum = n;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 2; j++) {
+				data[i][j] = scn.nextInt();
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int k = 0; k < n; k++) {
+				if (data[i][1] == data[k][0]) {
+					sum = sum - 1;
+				}
+			}
+		}
+		System.out.println(sum);
 	}
 
 }
